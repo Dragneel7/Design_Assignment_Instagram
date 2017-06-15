10dd7611b99fcecb966dafd8abd24113cf517db6
surya saini <sainisurya.1@gmail.com>
2017-06-15 16:54:49 +0530

    image view model modified

--- js/explore.js
+++ js/explore.js
@@ -9,3 +9,12 @@ function close1(){
 	
 	document.getElementById('ind_image').style.display="none";
 }
+function fun3(){
+	console.log("yes");
+	document.getElementById('pop').style.display="block";
+	document.getElementById('dots').style.display="none";
+}
+function close2(){
+	document.getElementById('pop').style.display="none";
+		document.getElementById('dots').style.display="block";
+}
