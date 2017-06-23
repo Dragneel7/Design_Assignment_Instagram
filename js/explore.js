b110adee67f8472563b8f185abf85df26f1041ee
surya saini <sainisurya.1@gmail.com>
2017-06-23 15:41:47 +0530

    profile page modified

--- js/explore.js
+++ js/explore.js
@@ -1,5 +1,6 @@
 function fun(id){
 	document.getElementById('overlay').style.display="block";
+
 	var x = document.getElementById(id).src;
 	document.getElementById('ind_image').style.display="block";
 	document.getElementById('ind_image1').src=x;
@@ -9,12 +10,16 @@ function fun(id){
 
 function fun3(){
 	console.log("yes");
+		document.getElementById('footer').style.zIndex="2";
+	document.getElementById('overlay2').style.display="block";
 	document.getElementById('pop1').style.display="block";
-	document.getElementById('dots').style.display="none";
+		document.getElementById('dots').style.display="none";
 	
 
 }
 function close2(){
+		document.getElementById('footer').style.zIndex="6";
+		document.getElementById('overlay2').style.display="none";
 	document.getElementById('pop1').style.display="none";
 		document.getElementById('dots').style.display="block";
 }
